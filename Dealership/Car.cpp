175550046017d591a79bf6038d63db8791574a4d
Eduard <ghiuzaneduard@gmail.com>
2022-11-09 21:06:03 +0200

    defined the explicit constructor and the destructor

--- Dealership/Car.cpp
+++ Dealership/Car.cpp
@@ -1,1 +1,25 @@
 #include "Car.h"
+#include <string.h>
+
+short int Car::instanceNumber = 0;
+
+Car::Car(
+	const char* make, 
+	const char* model, 
+	short int year, 
+	float price
+) : id(Car::instanceNumber++) 
+{
+	this->make = new char[strlen(make) + 1];
+	strcpy_s(this->make, strlen(make) + 1, make);
+	this->model = new char[strlen(model) + 1];
+	strcpy_s(this->model, strlen(model) + 1, model);
+	this->year = year;
+	this->price = price;
+}
+
+Car::~Car()
+{
+	delete[] make;
+	delete[] model;
+}

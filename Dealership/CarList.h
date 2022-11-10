8b260253a46ba2e1379efdc95689292b7ab3f6b1
Eduard <ghiuzaneduard@gmail.com>
2022-11-10 12:27:39 +0200

    added fields, constructor and methods declarations

--- Dealership/CarList.h
+++ Dealership/CarList.h
@@ -1,6 +1,17 @@
 #pragma once
+#include "Car.h"
 
 class CarList
 {
+public:
+	Car* data;
+	CarList* head;
+	CarList* tail;
+	CarList* next;
+	CarList* prev;
+
+	CarList();
+	void add(Car car);
+	void print();
 };
 

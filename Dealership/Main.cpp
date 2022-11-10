7313a587284299bbee0c2ba93ff3cbaa7ced25fc
Eduard <ghiuzaneduard@gmail.com>
2022-11-10 17:41:33 +0200

    slight changes to test the price parameter of Car constructor

--- Dealership/Main.cpp
+++ Dealership/Main.cpp
@@ -6,9 +6,9 @@
 int main() {
 
 	Dealership* dealership = new Dealership;
-	dealership->addCar(Car("BMW", "X6", 2010, 8200));
-	dealership->addCar(Car("Audi", "A4", 2009, 4500));
-	dealership->addCar(Car("Volkswagen", "Golf", 2012, 6000));
+	dealership->addCar(Car("BMW", "X6", 2010, 8200.755));
+	dealership->addCar(Car("Audi", "A4", 2009, 4500.123));
+	dealership->addCar(Car("Volkswagen", "Golf", 2012, 6000.799));
 	dealership->addCar(Car("BMW", "320i", 2016, 10000));
 	dealership->print();
 }

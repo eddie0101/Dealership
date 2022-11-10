7313aa6759973b4927a4c9f39698bb2d22a4978f
Eduard <ghiuzaneduard@gmail.com>
2022-11-10 12:44:12 +0200

    tested the Dealership class

--- Dealership/Main.cpp
+++ Dealership/Main.cpp
@@ -1,7 +1,14 @@
 #include "Car.h"
+#include "CarList.h"
+#include "Dealership.h"
 #include <iostream>
 
 int main() {
-	Car car("Audi", "A4", 2009, 4500);
-	std::cout << Car::instanceNumber << std::endl;
+
+	Dealership* dealership = new Dealership;
+	dealership->addCar(Car("BMW", "X6", 2010, 8200));
+	dealership->addCar(Car("Audi", "A4", 2009, 4500));
+	dealership->addCar(Car("Volkswagen", "Golf", 2012, 6000));
+	dealership->addCar(Car("BMW", "320i", 2016, 10000));
+	dealership->print();
 }

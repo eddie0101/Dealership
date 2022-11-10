3506f327ff6db3ce39d018268ab05609a4e0b739
Eduard <ghiuzaneduard@gmail.com>
2022-11-10 12:41:15 +0200

    added definitions

--- Dealership/Dealership.cpp
+++ Dealership/Dealership.cpp
@@ -1,1 +1,16 @@
 #include "Dealership.h"
+
+Dealership::Dealership()
+{
+	cars = new CarList;
+}
+
+void Dealership::addCar(Car car)
+{
+	cars->add(car);
+}
+
+void Dealership::print()
+{
+	cars->print();
+}
